ce918e732449490d5f6f47e06a66c9d9e01c0064
Qosdil A <qosdil@yahoo.com>
2023-11-30 19:00:12 +0700

    Create schema for movies table.

--- schema.sql
+++ schema.sql
@@ -1,1 +1,13 @@
--- Hello!
+CREATE TABLE
+  public.movies (
+    id serial NOT NULL,
+    title character varying(255) NOT NULL,
+    description text NULL,
+    rating smallint NOT NULL DEFAULT 0,
+    image character varying(255) NULL,
+    created_at timestamp without time zone NOT NULL DEFAULT now(),
+    updated_at timestamp without time zone NOT NULL DEFAULT now(),
+    deleted_at timestamp without time zone NULL
+  );
+
+ALTER TABLE public.movies ADD CONSTRAINT movies_pkey PRIMARY KEY (id);
